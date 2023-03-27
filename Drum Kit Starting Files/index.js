ea807e341cb3b07c49cff0f4fbbe0c65bc1904a9
Armando R <armando.roman.jr20@gmail.com>
2023-03-27 10:22:53 -0500

    Making sure all changes are pushed
     Please enter the commit message for your changes. Lines starting

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,8 +1,8 @@
 let numberOfDrumButtons = document.querySelectorAll(".drum").length;
 for (var i = 0; i<numberOfDrumButtons; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        this.style.color = "white";
-        console.log(this);
+        let audio = new Audio("sounds/tom-1.mp3");
+        audio.play();
     })
 }
 
