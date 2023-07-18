d64cdeaba7af71d1ccda18b8bae01d99cd5755f4
Armando R <armando.roman.jr20@gmail.com>
2023-07-18 09:43:35 -0500

    Pushing new files

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,11 @@
+import generateName from "sillyname";
+import superheroes from "superheroes";
+
+
+// var generateName = require("sillyname");
+// var sillyName = generateName();
+
+var superheroeName = superheroes.random();
+
+console.log(`my name is ${superheroeName}`);
+
