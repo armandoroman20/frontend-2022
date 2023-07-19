25f4f188c7b9b9b39f2301b5fa18ddf6b38d4dde
Armando R <armando.roman.jr20@gmail.com>
2023-07-19 16:30:49 -0500

    Pushing new files

--- Backend/3.1 Express Server/index.js
+++ Backend/3.1 Express Server/index.js
@@ -1,5 +1,10 @@
 import express from "express";
 const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.send("hello World");
+});
 
 app.listen(3000, () => {
     console.log(`Server irunning on port ${port}.`);
